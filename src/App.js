bdda85e212e5469276ecb998dd2d40aeeec73a40
Ali <thexali123@gmail.com>
2023-08-06 00:46:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
+import pic from './pn.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav'>
+
+        <a className='nav-links' href="/">Home</a>
+        <a className='nav-links' href="/">About</a>
+        <a className='nav-links' href="/">logo</a>
+        <a className='nav-links' href="/">Contact</a>
+        <a className='nav-links' href="/">Q&A</a>
+      </div>
+
+      <div className='home-img'>
+        <img src={pic} alt=''></img>
+      </div>
+
+
+      <h1 className='home-categories-h1' align="left"  >  Categories: </h1>
+
+      <div className='home-categories'>
+
+
+
+        <div className='home-categories-box'>
+
+          <h1>uoefh</h1>
+
+
+        </div>
+
+        <div className='home-categories-box'>
+          <h1>uoefh</h1>
+        </div>
+
+        <div className='home-categories-box'>
+          <h1>uoefh</h1>
+        </div>
+
+        <div className='home-categories-box'>
+          <h1>uoefh</h1>
+        </div>
+
+
+      </div>
+
+
+
+      <h1 className='home-brands-h1' align="left"  > Brands: </h1>
+
+
+
+      <div className='home-brands'>
+
+        <div className='home-brands-box'>
+
+          <h1>uoefh</h1>
+
+        </div>
+
+        <div className='home-brands-box'>
+          <h1>uoefh</h1>
+        </div>
+
+        <div className='home-brands-box'>
+          <h1>uoefh</h1>
+        </div>
+
+        <div className='home-brands-box'>
+          <h1>uoefh</h1>
+        </div>
+      </div>
+
+
+
+      <h1 className='home-topSellers-h1' align="left"  >  Top Sellers : </h1>
+
+      <div className='home-topSellers'>
+
+
+
+
+        <div className='home-topSellers-box'>
+
+          <h1>uoefh</h1>
+
+        </div>
+
+        <div className='home-topSellers-box'>
+          <h1>uoefh</h1>
+        </div>
+
+        <div className='home-topSellers-box'>
+          <h1>uoefh</h1>
+        </div>
+
+        <div className='home-topSellers-box'>
+          <h1>uoefh</h1>
+        </div>
+
+      </div>
+
     </div>
   );
 }
